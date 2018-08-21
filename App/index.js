9e8fdffdf145f82b672ece4194fa1bd590342109
John Hanlan <JohnJHanlan@gmail.com>
2018-08-20 19:55:07 -0500

    Created API screen and button to get to API screen

--- App/index.js
+++ App/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default class App extends React.Component {
   render () {
     return (
       <View style={styles.container}>
         <Text>Guild Pal</Text>
+        <Button 
+          title="Add Personal API Key Here"
+          onPress={() => this.props.navigation.navigate('EnterAPIKey') } 
+        />
       </View>
     )
   }
